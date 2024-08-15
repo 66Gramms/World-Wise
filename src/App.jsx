ed11ccef07c2f858d71102f9c58eca7bc031c8c7
66Gramms <meszarosricsi01@gmail.com>
2024-08-15 10:16:06 +0200

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
-  return <div>World wise</div>;
+  return (
+    <>
+      <h1>Hello Router!</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
