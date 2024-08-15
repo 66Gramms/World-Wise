bf032122d7959c8c55490f6719e61c24facfcee7
66Gramms <meszarosricsi01@gmail.com>
2024-08-15 11:10:15 +0200

    Add navigation

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 export default function Homepage() {
-  return <div>World wise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>World wise</h1>
+    </div>
+  );
 }
