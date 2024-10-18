75e87b8462af03835e4a3b61f8dca75e960944db
66Gramms <meszarosricsi01@gmail.com>
2024-10-18 08:45:22 +0200

    Protect routes that need authentication

---
+++ src/pages/ProtectecRoute.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
+
+export default function ProtectecRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+
+  return isAuthenticated ? children : null;
+}
